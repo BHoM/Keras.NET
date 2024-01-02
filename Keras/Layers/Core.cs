be1c9093684b8062ab6ad006fa4266510d843ccc
BHoMBot <BHoMBot@burohappold.com>
2024-01-02 14:21:07 +0000

    Uptick copyright headers to 2024

--- Keras/Layers/Core.cs
+++ Keras/Layers/Core.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -337,4 +337,5 @@ namespace Keras.Layers
         }
     }
 }
+
 
